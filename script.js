3894c0e65afce687cee5731556ac27bb61bdfbcc
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2024-05-01 19:17:25 +0200

    more panels crfeate index file etc

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin)
+    // gsap code here!
+    
+    
+  
+    let sections = gsap.utils.toArray(".panel");
+    
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".scene1",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        markers: true,
+        end: () => "+=" + document.querySelector(".container").offsetWidth
+      }
+      
+    });
+
+
+
+
+
+
+   });
